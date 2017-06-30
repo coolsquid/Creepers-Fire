5050a668efc8b8f35ac55b08c9581db4e293efa5
CoolSquid <mail@coolsquid.me>
2017-06-30 14:03:33 +0200

    Updated to Forge 14.21.1.2387, fixed errors

--- src/main/java/coolsquid/creepersfire/ConfigGuiFactory.java
+++ src/main/java/coolsquid/creepersfire/ConfigGuiFactory.java
@@ -15,17 +15,7 @@ public class ConfigGuiFactory implements IModGuiFactory {
 	}
 
 	@Override
-	public Class<? extends GuiScreen> mainConfigGuiClass() {
-		return Gui.class;
-	}
-
-	@Override
 	public Set<RuntimeOptionCategoryElement> runtimeGuiCategories() {
-		return null;
-	}
-
-	@Override
-	public RuntimeOptionGuiHandler getHandlerFor(RuntimeOptionCategoryElement element) {
 		return null;
 	}
 
